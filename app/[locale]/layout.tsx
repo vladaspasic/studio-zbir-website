1fa8e4dc2a009d4d2c4fca88d5d8949f41bd73b1
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2024-08-19 19:46:58 +0200

    Fix static pages build due to i18n

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from 'next';
+import type {Metadata} from 'next';
 import Link from 'next/link';
 import { NextIntlClientProvider } from 'next-intl';
-import { getMessages, getTranslations } from 'next-intl/server';
+import { getMessages, getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 import { Inter } from 'next/font/google';
 import { LOCALES } from 'zbir/i18n/locales';
 import '../globals.css';
@@ -14,8 +14,8 @@ interface LocaleParams {
     locale: string,
 }
 
-export async function generateMetadata({ params }: { params: LocaleParams }): Promise<Metadata> {
-    const t = await getTranslations({ locale: params.locale });
+export async function generateMetadata({params}: { params: LocaleParams }): Promise<Metadata> {
+    const t = await getTranslations({locale: params.locale});
 
     return {
         title: t('title'),
@@ -23,34 +23,37 @@ export async function generateMetadata({ params }: { params: LocaleParams }): Pr
 }
 
 export function generateStaticParams() {
-  return LOCALES.map((locale) => ({ locale }));
+    return LOCALES.map((locale) => ({locale}));
 }
 
-export default async function LocaleLayout({ children, params }: {
-  children: React.ReactNode;
-  params: LocaleParams;
+export default async function LocaleLayout({children, params}: {
+    children: React.ReactNode;
+    params: LocaleParams;
 }) {
-  const messages = await getMessages();
+    unstable_setRequestLocale(params.locale);
 
-  return (
-      <html lang={params.locale}>
-        <body className={inter.className}>
-          <NextIntlClientProvider messages={messages}>
-              <header className="container mx-auto my-4">
-                  <Link href={`/${params.locale}`} className="font-semibold leading-6 text-gray-800 hover:text-black transition-colors">
-                      Studio Zbir
-                  </Link>
-              </header>
+    const messages = await getMessages();
 
-              <main className="container mx-auto">
-                  {children}
-              </main>
+    return (
+        <html lang={params.locale}>
+            <body className={inter.className}>
+                <NextIntlClientProvider messages={messages}>
+                    <header className="container mx-auto my-4">
+                        <Link href={`/${params.locale}`}
+                              className="font-semibold leading-6 text-gray-800 hover:text-black transition-colors">
+                            Studio Zbir
+                        </Link>
+                    </header>
 
-              <footer>
+                    <main className="container mx-auto">
+                        {children}
+                    </main>
 
-              </footer>
-          </NextIntlClientProvider>
-        </body>
-      </html>
-  );
+                    <footer>
+
+                    </footer>
+                </NextIntlClientProvider>
+            </body>
+        </html>
+    );
 }
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,16 +1,19 @@
 import { useTranslations } from 'next-intl';
+import { unstable_setRequestLocale } from 'next-intl/server';
 
-export default function Home() {
-  const t = useTranslations();
+export default function Home({ params }: { params: { locale: string } }) {
+    unstable_setRequestLocale(params.locale);
 
-  return (
-      <div>
-        <h1 className="text-3xl font-bold laptop:text-6xl mb-5">
-          {t('lead.title')}
-        </h1>
-        <p className="text-xl font-semibold laptop:text-3xl">
-          {t('lead.subtitle')}
-        </p>
-      </div>
-  );
+    const t = useTranslations();
+
+    return (
+        <div>
+            <h1 className="text-3xl font-bold laptop:text-6xl mb-5">
+                {t('lead.title')}
+            </h1>
+            <p className="text-xl font-semibold laptop:text-3xl">
+                {t('lead.subtitle')}
+            </p>
+        </div>
+    );
 }
